0260f0a65c67cb3f9c73e7fad452fe3065751820
bolajahmad <corpbjahmad@gmail.com>
2022-05-05 12:05:49 +0100

    dealing with numbers and complex numbers

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
+use num::complex::Complex;
+
 fn main() {
-    let a = 20;
-    let b: i32 = 30;
+    let a = Complex { re: 2.1, im: 5.0 };
+    let b = Complex::new(1.1, 2.2);
     
-    let addition = a + b;
-    println!("{} + {} = {}", a, b, addition);
+    let result = a + b;
 
-    let d: i64 = 1_000_000;
-    println!("{}", d.pow(2));
-    println!("{} + {} = {}", a, d, (a + d));
-
-    let e =  [
-        5.0,
-        5f32,
-        5.0_f32
-    ];
-    println!("{:3}", e[1]);
+    println!("{} + {}i", result.re, result.im);
 }
