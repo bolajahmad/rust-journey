1b066477f532945194ac55a9e2018eccb5ce9686
bolajahmad <corpbjahmad@gmail.com>
2022-05-15 15:50:18 +0100

    string

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
-use num::complex::Complex;
+// use num::complex::Complex;
 
 fn main() {
-    let a = Complex { re: 2.1, im: 5.0 };
-    let b = Complex::new(1.1, 2.2);
-    
-    let result = a + b;
+    let search_term = "jangilo";
 
-    println!("{} + {}i", result.re, result.im);
+    let quote = "\
+    Every man shall die, and it's no joke
+    dark square is a picture with no frames
+    Its looks like a jangilova";
+
+    // let mut line_num: usize = 1;
+
+    // for line in quote.lines() {
+    //     if line.contains(search_term) {
+    //         println!("{}, line: {}", line, line_num);
+    //     }
+    //     line_num += 1;
+    // }
+
+    for (i, line) in quote.lines().enumerate() {
+        if line.contains(search_term) {
+            println!("{}, line: {}", line, i + 1);
+        }
+    }
 }
