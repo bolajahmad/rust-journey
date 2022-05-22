d96f9981e034b7ce328f9c312116c26e890aa010
bolajahmad <corpbjahmad@gmail.com>
2022-05-22 11:02:04 +0100

    first vector code

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,54 @@
 // use num::complex::Complex;
 
 fn main() {
-    let one = [1, 2, 3];
-    let two: [u8; 3] = [1, 2, 3];
-    let blank1 = [0; 3];
-    let blank2: [u8; 3] = [0; 3];
+    let ctx_lines = 2;
+    let needle = "oo";
+    let haystack = "\
+        Every face, every shop,
+        bedroom window, public-house, and
+        dark square is a picture
+        feverishly turned--in search of what?
+        It is the same with books.
+        What do we seek
+        through millions of pages?
+    ";
 
-    let arrays: [[u8; 3]; 4] = [one, two, blank1, blank2];
+    let mut tags: Vec<usize> = vec![];
+    let mut ctx: Vec<Vec<(usize, String)>> = vec![];
 
-    for a in &arrays {
-        print!("{:?}: ", a);
-        for n in a.iter() {
-            print!("\t{} + 10 = {}", n, n + 10);
+    for (i, line) in haystack.lines().enumerate() {
+        if  line.contains(needle) {
+            tags.push(i);
+
+            // reserved space for n items
+            let v = Vec::with_capacity(2*ctx_lines + 1);
+            ctx.push(v);
         }
+    }
 
-        let mut sum = 0;
-        for i in 0..a.len() {
-            sum += a[i];
+    println!("available tags {:?}, {}", tags, ctx[0].capacity());
+
+    if tags.is_empty() {
+        return;
+    }
+
+    for (i, line) in haystack.lines().enumerate() {
+        for (j, tag) in tags.iter().enumerate() {
+            let lower_bound = tag.saturating_sub(ctx_lines);
+            let upper_bound = tag + ctx_lines;
+
+            if (i >= lower_bound) && (i <= upper_bound) {
+                let line_as_string = String::from(line);
+                let local_ctx = (i, line_as_string);
+                ctx[j].push(local_ctx);
+            }
         }
-        println!("\t({:?} = {})", a, sum);
+    }
+
+    for local_ctx in ctx.iter() {
+        for &(i, ref line) in local_ctx.iter() {
+            let line_num = i + 1;
+            println!("{}: {}", line_num, line);
+        }
     }
 }
