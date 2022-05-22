e43d05bfcf9fc6922b2f8c41e55f8db486a141e7
bolajahmad <corpbjahmad@gmail.com>
2022-05-22 09:34:15 +0100

    lists

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
 // use num::complex::Complex;
 
 fn main() {
-    let search_term = "jangilo";
+    let one = [1, 2, 3];
+    let two: [u8; 3] = [1, 2, 3];
+    let blank1 = [0; 3];
+    let blank2: [u8; 3] = [0; 3];
 
-    let quote = "\
-    Every man shall die, and it's no joke
-    dark square is a picture with no frames
-    Its looks like a jangilova";
+    let arrays: [[u8; 3]; 4] = [one, two, blank1, blank2];
 
-    // let mut line_num: usize = 1;
-
-    // for line in quote.lines() {
-    //     if line.contains(search_term) {
-    //         println!("{}, line: {}", line, line_num);
-    //     }
-    //     line_num += 1;
-    // }
-
-    for (i, line) in quote.lines().enumerate() {
-        if line.contains(search_term) {
-            println!("{}, line: {}", line, i + 1);
+    for a in &arrays {
+        print!("{:?}: ", a);
+        for n in a.iter() {
+            print!("\t{} + 10 = {}", n, n + 10);
         }
+
+        let mut sum = 0;
+        for i in 0..a.len() {
+            sum += a[i];
+        }
+        println!("\t({:?} = {})", a, sum);
     }
 }
