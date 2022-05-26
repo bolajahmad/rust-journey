7597f17d88d7d9853569f1d467900fdb81010ca1
bolajahmad <corpbjahmad@gmail.com>
2022-05-26 10:46:16 +0100

    isolated line processing logic and added stdin

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use regex::Regex;
 use clap::{App, Arg};
 use std::fs::File;
+use std::io;
 use std::io::BufReader;
 use std::io::prelude::*;
 
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => println!("No matches were found!"),
+        }
+    }
+}
 fn main() {
     let args = App::new("grep-lite")
         .version("0.1")
@@ -12,19 +23,24 @@ fn main() {
             .help("The pattern to search for")
             .takes_value(true)
             .required(true))
+        .arg(Arg::with_name("input")
+            .help("File to search")
+            .takes_value(true)
+            .required(false))
         .get_matches();
+
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
+    let input = args.value_of("input").unwrap_or("-");
 
-    let file = File::open("Cargo.toml").unwrap();
-    let buf_reader = BufReader::new(file);
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
 
-    for line_ in buf_reader.lines() {
-        let line = line_.unwrap();
-
-        match re.find(&line) {
-            Some (_) => println!("{}", line),
-            None => (),
-        }
+        process_lines(reader, re);
+    } else {
+        let file = File::open(input).unwrap();
+        let reader = BufReader::new(file);
+        process_lines(reader, re);
     }
 }
