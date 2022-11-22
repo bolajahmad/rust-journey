fd1450815f037ea134ad93ef20571a89d839a995
Bolaji Ahmad <56865496+bolajahmad@users.noreply.github.com>
2022-11-22 21:12:00 +0100

    implemented guess a number

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!!");
 
-    println!("Please input your number");
+    let secret_number = rand::thread_rng().gen_range(1..10);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your number");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: i32 = guess.trim().parse().expect("Please parse a number");
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("Guessed right");
+                break;
+            }
+            Ordering::Less => println!("Guess is lower"),
+            Ordering::Greater => println!("Guess is higher"),
+        }
+    }
+    println!("The secret number is: {secret_number}");
 }
