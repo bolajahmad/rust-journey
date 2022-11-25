aa3a891062c6df6ef1a4a986d2aacecc5352f6a4
Bolaji Ahmad <56865496+bolajahmad@users.noreply.github.com>
2022-11-25 10:58:48 +0100

    struct

--- test_one/src/main.rs
+++ test_one/src/main.rs
@@ -1,18 +1,19 @@
 fn main() {
-    println!("Fibonacci Printer");
-
-    let test_value = 13;
-    let tester = fibonacci(test_value);
-    println!("Fibonacci of {test_value} is {tester}");
+    let rectangle = Rectangle {
+        width: dbg!(20),
+        height: 5,
+    };
+    // dbg!(&rectangle);
+    println!("rectangle is {:#?}", rectangle);
+    calculate_area(&rectangle);
 }
 
-// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181
-fn fibonacci(value: i64) -> i64 {
-    if value == 0 {
-        return 0;
-    } else if value == 1 {
-        return 1;
-    } else {
-        fibonacci(value - 1) + fibonacci(value - 2)
-    }
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn calculate_area(rectangle: &Rectangle) -> u32 {
+    rectangle.height * rectangle.width
 }
