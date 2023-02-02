13b85e8dae3a64aa60004c542214829a7935a668
Bolaji Ahmad <56865496+bolajahmad@users.noreply.github.com>
2023-02-02 10:04:29 +0100

    abstracting function logic

---
+++ generics/target/debug/deps/generics-a465a712087fa0ac.d
@@ -0,0 +1,5 @@
+/Users/bolajahmad/Documents/fun/rust-journey/generics/target/debug/deps/generics-a465a712087fa0ac: src/main.rs
+
+/Users/bolajahmad/Documents/fun/rust-journey/generics/target/debug/deps/generics-a465a712087fa0ac.d: src/main.rs
+
+src/main.rs:
---
+++ generics/target/debug/generics.d
@@ -0,0 +1,1 @@
+/Users/bolajahmad/Documents/fun/rust-journey/generics/target/debug/generics: /Users/bolajahmad/Documents/fun/rust-journey/generics/src/main.rs
