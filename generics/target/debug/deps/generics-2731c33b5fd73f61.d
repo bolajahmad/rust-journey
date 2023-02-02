31308a11a4f614953c007b4fd6f842d05aca1711
Bolaji Ahmad <56865496+bolajahmad@users.noreply.github.com>
2023-02-02 10:32:26 +0100

    generics with enums and structs

---
+++ generics/target/debug/deps/generics-2731c33b5fd73f61.d
@@ -0,0 +1,5 @@
+/Users/bolajahmad/Documents/fun/rust-journey/generics/target/debug/deps/generics-2731c33b5fd73f61.rmeta: src/main.rs
+
+/Users/bolajahmad/Documents/fun/rust-journey/generics/target/debug/deps/generics-2731c33b5fd73f61.d: src/main.rs
+
+src/main.rs:
---
+++ generics/target/debug/deps/generics-a914217945941c35.d
@@ -0,0 +1,5 @@
+/Users/bolajahmad/Documents/fun/rust-journey/generics/target/debug/deps/generics-a914217945941c35.rmeta: src/main.rs
+
+/Users/bolajahmad/Documents/fun/rust-journey/generics/target/debug/deps/generics-a914217945941c35.d: src/main.rs
+
+src/main.rs:
