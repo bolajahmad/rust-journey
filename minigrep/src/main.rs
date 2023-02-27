419f8d9e750039e37c6ba43411af1a9a198ac9ce
Bolaji Ahmad <56865496+bolajahmad@users.noreply.github.com>
2023-02-27 19:49:09 +0100

    error handling with panic

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,19 +5,33 @@ fn main() {
     // fetch the arguments passed to the command line when cargo run is called
     let args: Vec<String> = env::args().collect();
 
-    let (_query, file_path) = parse_config(&args);
+    let config = Config::new(&args);
 
     // read content of the file_path
-    let file_content = fs::read_to_string(file_path).expect("Should be able to read the file");
+    let file_content =
+        fs::read_to_string(&config.file_path).expect("Should be able to read the file");
     println!("With text: \n{file_content}");
 
     // dbg!(args);
 }
 
-fn parse_config(args: &Vec<String>) -> (&String, &String) {
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("Searching for {query}");
-    println!("In file, {file_path}");
-    (query, file_path)
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough command line arguments");
+        }
+        let query = &args[1];
+        let file_path = &args[2];
+        println!("Searching for {query}");
+        println!("In file, {file_path}");
+        Config {
+            query: query.to_string(),
+            file_path: file_path.to_string(),
+        }
+    }
 }
